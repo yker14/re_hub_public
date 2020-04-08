1571e5cf084666f47b9352ff2dc3c1022058811f
Yuseff <ykr20@outlook.com>
2020-04-07 20:09:15 -0500

    main setup ready

--- view/Shell.controller.js
+++ view/Shell.controller.js
@@ -1,31 +1,31 @@
 sap.ui.define([
 	'sap/ui/core/mvc/Controller',
 	'sap/ui/model/json/JSONModel',
-    'sap/m/MessageBox',
-    'rshub/ui/libs/custom/Utilities',
-    'sap/m/Popover',
-    'sap/m/Button',
+  'sap/m/MessageBox',
+  'rshub/ui/libs/custom/Utilities',
+  'sap/m/Popover',
+  'sap/m/Button',
 	'sap/m/library',
-    'sap/ui/core/Popup',
-    'sap/ui/core/Fragment',
+  'sap/ui/core/Popup',
+  'sap/ui/core/Fragment',
 	'sap/ui/core/routing/History',
 	'sap/ui/core/UIComponent'
 ], function (Controller, JSONModel, MessageBox, Utils, Popover, Button, Library, Popup, Fragment, History, UIComponent) {
 	"use strict";
-    
+
 	var CController = Controller.extend(Utils.nameSpaceHandler("controller.Shell"), {
-                
+
         onInit : function () {
-            
+
             //Global variables for Controller
             this.addedPages = {};
-            
+
             this.oModel = new JSONModel();
-			this.oModel.loadData(sap.ui.require.toUrl("rshub/ui/model/") + "/pages.json", null, false);
+						this.oModel.loadData(sap.ui.require.toUrl("rshub/ui/model/") + "/pages.json", null, false);
             this.getView().setModel(this.oModel);
 		},
-        
-        getRouter : function () {
+
+    getRouter : function () {
 			return UIComponent.getRouterFor(this);
 		},
 
@@ -41,7 +41,7 @@ sap.ui.define([
 				this.getRouter().navTo("Homeview", {}, true /*leave no history for browser*/);
 			}
 		},
-        
+
         /*
         handleMenuPress: function(oEvent) {
 			var oButton = oEvent.getSource();
@@ -63,14 +63,14 @@ sap.ui.define([
 			}
 		},
         */
-        
+
         onMenuItemSelect : function(ev) {
-            
+
             this.getRouter().navTo(ev.getParameter("item").getProperty("key"))
-            
+
             console.log('NavMenu Dialog Item Selected');
 		},
-        
+
         handleNamePress: function (event) {
 			var oPopover = new Popover({
 				showHeader: false,
@@ -92,9 +92,20 @@ sap.ui.define([
 			}).addStyleClass('sapMOTAPopover sapTntToolHeaderPopover');
 
 			oPopover.openBy(event.getSource());
+		},
+
+		onSideNavButtonPress: function () {
+			var oToolPage = this.byId("toolPage");
+			var bSideExpanded = oToolPage.getSideExpanded();
+            var oToggleButton = this.byId('sideNavigationToggleButton');
+
+            (bSideExpanded) ?   oToggleButton.setTooltip('Expandir') :
+                                oToggleButton.setTooltip('Colapsar');
+
+			oToolPage.setSideExpanded(!oToolPage.getSideExpanded());
 		}
-        
-	
+
+
 	});
 
 
